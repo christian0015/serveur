a177dd76c227df1b2eaf1cb589f7407686907deb
Christian TUKUNDA <134978994+christian0015@users.noreply.github.com>
2023-10-24 14:54:01 +0100

    Add files via upload

---
+++ client.js
@@ -0,0 +1,31 @@
+const socket = io();
+
+const username = prompt('Entrez votre nom d\'utilisateur :');
+socket.emit('user-connected', username);
+
+const userList = document.getElementById('users');
+const messages = document.getElementById('messages');
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('sendButton');
+
+socket.on('update-user-list', (usernames) => {
+    userList.innerHTML = '';
+    usernames.forEach((user) => {
+        const li = document.createElement('li');
+        li.textContent = user;
+        userList.appendChild(li);
+    });
+});
+
+sendButton.addEventListener('click', () => {
+    const to = prompt('Entrez le nom de l\'utilisateur avec lequel vous voulez communiquer :');
+    const message = messageInput.value;
+    socket.emit('send-message', { to, message });
+    messageInput.value = '';
+});
+
+socket.on('message', (data) => {
+    const message = document.createElement('li');
+    message.textContent = `${data.from}: ${data.message}`;
+    messages.appendChild(message);
+});
